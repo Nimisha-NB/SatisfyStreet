345771aa5b50ef3073cfe34385a505de37daa1f0
Manas Hejmadi <manas2all@outlook.com>
2024-02-06 00:52:04 +0530

    Raqeeb USN added

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -61,7 +61,7 @@ const LoginPage = () => {
         <div>
             <img src="/dsce.png" alt="" />
             <p>Semester 3 WebTech Project</p>
-            <p>made by <div className='person'>Nimisha NB</div>(CS140), <div className='person'>Manas Hejmadi</div>(CS117),<div className='person'> Manish S</div>(CS119), <div className='person'>Raqeeb Haider</div>(CS)</p>
+            <p>made by <div className='person'>Nimisha NB</div>(CS140), <div className='person'>Manas Hejmadi</div>(CS117),<div className='person'> Manish S</div>(CS119), <div className='person'>Raqeeb Haider</div>(CS126)</p>
             <h1><strong>SatisfyStreet</strong></h1>
             <input type="text" placeholder='Username' value={uname} onChange={(e) => setUname(e.target.value)} />
             <input type="password" placeholder='Password' value={pwd} onChange={(e) => setPwd(e.target.value)} />
